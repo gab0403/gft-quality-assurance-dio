e06aa57874d654ca7d85fb6ae3f381d6653a7a04
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-11-15 21:58:06 -0300

    feat: exercicios de operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,15 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "JAVA";
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+        numero = - numero;
+
+        System.out.println(numero);
+
+        numero = numero * -1;
+
+        System.out.println("Assim se converte para positivo novamente: " + numero);
+    }
+}
