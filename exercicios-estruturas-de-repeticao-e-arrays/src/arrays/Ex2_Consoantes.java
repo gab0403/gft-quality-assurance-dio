3a5d188ebb806623c046376868a4f5b6587a798c
Gabriela <gabriela0403rodriguesdossanto@gmail.com>
2022-11-23 21:05:57 -0300

    feat: Exercícios de estruturas de repetição e arrays

--- exercicios-estruturas-de-repeticao-e-arrays/src/arrays/Ex2_Consoantes.java
+++ exercicios-estruturas-de-repeticao-e-arrays/src/arrays/Ex2_Consoantes.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
  * Faça um Programa que leia um vetor de 6 caracteres,
  * e diga quantas consoantes foram lidas. Imprima as consoantes.
  */
+// consoantes
 public class Ex2_Consoantes {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
